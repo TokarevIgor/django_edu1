af2b0f9d0c22f92ad2dbd2c4273b4ed11e05f115
tokarev_igor <tokarev.igor93@gmail.com>
2021-12-10 20:02:56 +0600

    step 2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 
+menu_links = [
+    {'view_name':'main', 'name': 'домой'},
+    {'view_name':'products', 'name': 'продукты'},
+    {'view_name':'contact', 'name': 'контакты'}
+]
+
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context={
+        'title' : 'Магазин',
+        'menu_links': menu_links
+    })
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', context={
+        'title' : 'Каталог',
+        'menu_links': menu_links
+    })
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context={
+        'title' : 'Контакты',
+        'menu_links': menu_links
+    })
