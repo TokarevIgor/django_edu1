573659835fea3f7bfc610d4f996d7dd34aa3b4ea
tokarev_igor <tokarev.igor93@gmail.com>
2021-12-24 14:09:07 +0600

    lesson 6

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,11 +3,33 @@ from django.shortcuts import get_object_or_404, render
 from basketapp.models import Basket
 from .models import ProductCategory, Product
 
+import random
+
 MENU_LINKS = [
     {'view_name': 'main', 'name': 'домой'},
     {'view_name': 'products:index', 'name': 'продукты'},
     {'view_name': 'contact', 'name': 'контакты'}
 ]
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+        
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+    
+    
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).\
+                                    exclude(pk=hot_product.pk)[:3]
+
+    return same_products
+
 
 
 def main(request):
@@ -18,41 +40,12 @@ def main(request):
     })
 
 
-def products(request, pk=None):
-    title = 'Каталог'
-    links_menu = ProductCategory.objects.all()
-    basket = {
-            'basket': [],
-            'quantity': 0,
-            'total': 0
-        }
-    if request.user.is_authenticated:
-        basket_filter = Basket.objects.filter(user=request.user)
-        basket = {
-            'basket': basket_filter,
-            'quantity': Basket.quantityBasket(basket_filter),
-            'total': Basket.totalPriceBasket(basket_filter)
-        }
-
-    if pk is None:
-        pk = 0
-
-    if request.resolver_match.view_name == 'products:product' and pk != 0:
-        product = get_object_or_404(Product, pk=pk)
-        same_products = Product.objects.filter(category=product.category)[:3]
-
-        content = {
-            'title': product.name,
-            'menu_links': MENU_LINKS,
-            'ls_category': links_menu,
-            'same_products': same_products,
-            'product': product,
-            'basket': basket,
-        }
-
-        return render(request, 'mainapp/products.html', content)
-
-    else:
+def products(request, pk=None):    
+    title = 'продукты'
+    links_menu_category = ProductCategory.objects.all()
+    basket = get_basket(request.user)
+            
+    if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by('price')
             category = {'name': 'все'}
@@ -63,13 +56,28 @@ def products(request, pk=None):
         content = {
             'title': title,
             'menu_links': MENU_LINKS,
-            'ls_category': links_menu,
+            'links_menu_category': links_menu_category,
             'category': category,
             'products': products,
             'basket': basket,
         }
 
-        return render(request, 'mainapp/products_list.html', content)   
+        return render(request, 'mainapp/products_list.html', content)
+
+    
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+    
+    content = {
+        'title': title,
+        'menu_links': MENU_LINKS,
+        'links_menu_category': links_menu_category, 
+        'hot_product': hot_product,
+        'same_products': same_products,
+        'basket': basket,
+    }
+    
+    return render(request, 'mainapp/products.html', content)   
     
 
 
@@ -78,3 +86,17 @@ def contact(request):
         'title': 'Контакты',
         'menu_links': MENU_LINKS
     })
+
+def product(request, pk):
+
+    product = get_object_or_404(Product, pk=pk)
+        
+    content = {
+        'title': product.name, 
+        'menu_links': MENU_LINKS,
+        'links_menu_category': ProductCategory.objects.all(), 
+        'product': product, 
+        'basket': get_basket(request.user),
+    }
+	
+    return render(request, 'mainapp/product.html', content)
