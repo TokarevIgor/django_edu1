46309e3879756d43becab01c377acd6bc0ebef5e
tokarev_igor <tokarev.igor93@gmail.com>
2022-01-18 16:00:23 +0600

    add user profile

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserChangeForm
 from django import forms
-from .models import ShopUser
+from .models import ShopUser, ShopUserProfile
 import random
 import hashlib
 
@@ -70,3 +70,13 @@ class ShopUserEditForm(UserChangeForm):
             raise forms.ValidationError("Вы слишком молоды!")
 
         return data
+
+class ShopUserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = ShopUserProfile
+        fields = ('tagline', 'aboutMe', 'gender')
+
+    def __init__(self, *args, **kwargs):
+        super(ShopUserProfileEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
